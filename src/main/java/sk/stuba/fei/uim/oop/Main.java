df055a5e159d3c1c8cfd6bf4b6be37fb361a71d6
Eduard Zelenay <xzelenay@stuba.sk>
2022-03-16 15:45:15 +0100

    set

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -2,40 +2,31 @@ package sk.stuba.fei.uim.oop;
 
 import sk.stuba.fei.uim.oop.zvierata.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         List<Zviera> zveri = new ArrayList<>(List.of(new Pes("Dunco"),new Macka("Murko")));
 
-        Map<String, String> hlavneMesta = new HashMap<>();
-        hlavneMesta.put("Slovensko","Bratislava");
-        hlavneMesta.put("Cesko","Praha");
-        hlavneMesta.put("Polsko","Varsava");
-        hlavneMesta.put("Madarsko","Budapest");
+        Set<String> auta = new HashSet<>();
+        auta.add("Volvo");
+        auta.add("BMW");
+        auta.add("VW");
+        auta.add("Audi");
+        auta.add("Volvo");
+        auta.add("BMW");
 
-        System.out.println(hlavneMesta.size());
+        System.out.println(auta);
+        System.out.println(auta.size());
 
-        if(hlavneMesta.containsKey("Slovensko")){
-            hlavneMesta.remove("Slovensko");
+        if (auta.contains("Volvo")){
+            auta.remove("Volvo");
         }
-        for(String kluc : hlavneMesta.keySet()){
-            System.out.println(kluc);
-            System.out.println(hlavneMesta.get(kluc));
-        }
-        System.out.println("---");
-        for(String hodnota : hlavneMesta.values()){
-            System.out.println(hodnota);
-        }
-        System.out.println("---");
-        System.out.println(hlavneMesta);
-        hlavneMesta.clear();
-        System.out.println(hlavneMesta);
+        List<String> auta2 = new ArrayList<>(List.of("Volvo","BMW"));
 
-
+        auta.removeAll(auta2);
+        System.out.println(auta);
+        auta.clear();
         }
 
 }
