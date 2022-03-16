9402dc0b8c19de2c06ee70448954519f59d1b408
Eduard Zelenay <xzelenay@stuba.sk>
2022-03-16 15:28:26 +0100

    exceptions

---
+++ src/main/java/sk/stuba/fei/uim/oop/zvierata/Zviera.java
@@ -0,0 +1,19 @@
+package sk.stuba.fei.uim.oop.zvierata;
+
+public abstract class Zviera {
+    protected final String meno;
+
+    public Zviera(String meno) {
+        this.meno = meno;
+    }
+
+    public abstract void zvukZvierata();
+
+    public void pohladkat() {
+        System.out.println(this.meno + " bol pohladkany");
+        this.zvukZvierata();
+    }
+    public String getMeno() {
+        return meno;
+    }
+}
